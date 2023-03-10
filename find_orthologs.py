2914cbf5c783b63bba3eb8d0ce94dd72bf4fac3c
Malcolm Fisher <malcolm.fisher@cchmc.org>
2023-03-10 15:01:54 -0500

    Removed unneeded IF logic.

--- find_orthologs.py
+++ find_orthologs.py
@@ -20,40 +20,37 @@ def get_reciprocal_hits(file_one,file_two):
     output_list_2 = []      #Inversing the list in order to find common things bw output_list_1 and output_list_2
     output_list = []        #Contains only reciprocal blast hits
     
-    if not file_one:
-        print('No input file specified')
-    else:
-        file_1_db = "diamond makedb --in " + str(file_one) + " -d tmp/db1"   #Creating db for input1
-        file_1_db_subprocess = subprocess.check_output(file_1_db.split())
+    file_1_db = "diamond makedb --in " + str(file_one) + " -d tmp/db1"   #Creating db for input1
+    file_1_db_subprocess = subprocess.check_output(file_1_db.split())
+    
+    result_1 = "diamond blastp -d tmp/db1.dmnd -q " + str(file_two) + " --iterate --very-sensitive --max-target-seqs 1 --max-hsps 1 --outfmt 6 --out tmp/output_file_1" #Output for query = input2 and db = input1
+    result_1_subprocess = subprocess.check_output(result_1.split())
+    
+    file_2_db = "diamond makedb --in " + str(file_two) + " -d tmp/db2"   #Creating db for input2
+    file_2_db_subprocess = subprocess.check_output(file_2_db.split())
+    
+    result_2 = "diamond blastp -d tmp/db2.dmnd -q " + str(file_one) + " --iterate --very-sensitive --max-target-seqs 1 --max-hsps 1 --outfmt 6 --out tmp/output_file_2" #Output for query = input1 and db = input2
+    result_2_subprocess = subprocess.check_output(result_2.split())
+    
+    with open("tmp/output_file_1") as fh1:
+        for line in fh1.readlines():
+            a.append(line.split()[0])
+            a1.append(line.split()[1])
+                
+    for i,j in zip(a,a1):
+        output_list_1.append(i+"\t"+j+"\n")
         
-        result_1 = "diamond blastp -d tmp/db1.dmnd -q " + str(file_two) + " --iterate --very-sensitive --max-target-seqs 1 --max-hsps 1 --outfmt 6 --out tmp/output_file_1" #Output for query = input2 and db = input1
-        result_1_subprocess = subprocess.check_output(result_1.split())
+    with open("tmp/output_file_2") as fh2:
+        for line in fh2.readlines():
+            b.append(line.split()[0])
+            b1.append(line.split()[1])
+                
+    for i,j in zip(b,b1):
+        output_list_2.append(j+"\t"+i+"\n")
         
-        file_2_db = "diamond makedb --in " + str(file_two) + " -d tmp/db2"   #Creating db for input2
-        file_2_db_subprocess = subprocess.check_output(file_2_db.split())
-        
-        result_2 = "diamond blastp -d tmp/db2.dmnd -q " + str(file_one) + " --iterate --very-sensitive --max-target-seqs 1 --max-hsps 1 --outfmt 6 --out tmp/output_file_2" #Output for query = input1 and db = input2
-        result_2_subprocess = subprocess.check_output(result_2.split())
-        
-        with open("tmp/output_file_1") as fh1:
-            for line in fh1.readlines():
-                a.append(line.split()[0])
-                a1.append(line.split()[1])
-                    
-        for i,j in zip(a,a1):
-            output_list_1.append(i+"\t"+j+"\n")
-            
-        with open("tmp/output_file_2") as fh2:
-            for line in fh2.readlines():
-                b.append(line.split()[0])
-                b1.append(line.split()[1])
-                    
-        for i,j in zip(b,b1):
-            output_list_2.append(j+"\t"+i+"\n")
-            
-        for x in output_list_1:         #Checking common hits beween output_list_1 and output_list_2
-            if x in output_list_2:
-                output_list.append(x)   #Contains only reciprocal blast hits
+    for x in output_list_1:         #Checking common hits beween output_list_1 and output_list_2
+        if x in output_list_2:
+            output_list.append(x)   #Contains only reciprocal blast hits
     return(output_list)
 
 def main():
@@ -74,7 +71,7 @@ def main():
     '''
     output_list is a list of reciprocal BLAST hits. Each element is a tab
     separated pair of gene names. Eg:
-    ["A0A5S6MBM4|A0A5S6MBM4_XENTR	XBXT10g000564|mRNA002866|runx1t1", "A0A6I8Q0X6|A0A6I8Q0X6_XENTR	XBXT10g012953|mRNA064117|ano10", "A0A6I8Q7J7|A0A6I8Q7J7_XENTR	XBXT10g020004|mRNA098774|LOC101733477", ...]
+    ["gene|AM421808.1_cds_CAM09336.1_10	gene|AE002098.2_cds_NMB0033_33", "gene|AM421808.1_cds_CAM09337.1_11	gene|AE002098.2_cds_NMB0034_34", "gene|AM421808.1_cds_CAM09338.1_12	gene|AE002098.2_cds_NMB0035_35", "gene|AM421808.1_cds_CAM09339.1_13	gene|AE002098.2_cds_NMB0036_36", ...]
     '''
 
     make_dir = "mkdir tmp"
