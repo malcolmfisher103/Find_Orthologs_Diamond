12356ce9bd1e617b1b7e91ed07dc1a62dc685c40
Malcolm Fisher <malcolm.fisher@cchmc.org>
2023-03-10 13:58:03 -0500

    Updated to use Diamond aligner.

--- find_orthologs.py
+++ find_orthologs.py
@@ -1,18 +1,17 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3.8
 
 '''
 Script for finding orthologs using reciprocal BLAST hits.
 
 Sript usage is as follows:
-    ./find_orthologs.py -i1 <Input file 1> -i2 <Input file 2> -o <Output file name> –t <Sequence type – n/p>
+    ./find_orthologs.py -i1 <Input file 1> -i2 <Input file 2> -o <Output file name>
 
-where "n" specifies a nucleotide sequence and "p" specifies a protein sequence.
 '''
 import argparse
 import subprocess
 
-def get_reciprocal_hits(file_one,file_two, input_sequence_type):
-
+def get_reciprocal_hits(file_one,file_two):
+    
     a = []                  #List a stores the first index headers (db values)
     a1 = []                 #List a1 stores the second index headers (query headers)
     b = []                  #List b stores the first index headers (db values)
@@ -20,84 +19,41 @@ def get_reciprocal_hits(file_one,file_two, input_sequence_type):
     output_list_1 = []
     output_list_2 = []      #Inversing the list in order to find common things bw output_list_1 and output_list_2
     output_list = []        #Contains only reciprocal blast hits
-
-    if input_sequence_type == "n":  #For nucleotide(n)
-
-        file_1_db = "makeblastdb -in " + str(file_one) + " -dbtype nucl -out tmp/db1"   #Creating db for input1
+    
+    if not file_one:
+        print('No input file specified')
+    else:
+        file_1_db = "diamond makedb --in " + str(file_one) + " -d tmp/db1"   #Creating db for input1
         file_1_db_subprocess = subprocess.check_output(file_1_db.split())
-
-        result_1 = "blastn -db tmp/db1 -query " + str(file_two) + " -max_target_seqs 1 -max_hsps 1 -outfmt 6 -out tmp/output_file_1" #Output for query = input2 and db = input1
+        
+        result_1 = "diamond blastp -d tmp/db1.dmnd -q " + str(file_two) + " --iterate --very-sensitive --max-target-seqs 1 --max-hsps 1 --outfmt 6 --out tmp/output_file_1" #Output for query = input2 and db = input1
         result_1_subprocess = subprocess.check_output(result_1.split())
-
-        file_2_db = "makeblastdb -in " + str(file_two) + " -dbtype nucl -out tmp/db2"   #Creating db for input2
+        
+        file_2_db = "diamond makedb --in " + str(file_two) + " -d tmp/db2"   #Creating db for input2
         file_2_db_subprocess = subprocess.check_output(file_2_db.split())
-
-        result_2 = "blastn -db tmp/db2 -query " + str(file_one) + " -max_target_seqs 1 -max_hsps 1 -outfmt 6 -out tmp/output_file_2" #Output for query = input1 and db = input2
+        
+        result_2 = "diamond blastp -d tmp/db2.dmnd -q " + str(file_one) + " --iterate --very-sensitive --max-target-seqs 1 --max-hsps 1 --outfmt 6 --out tmp/output_file_2" #Output for query = input1 and db = input2
         result_2_subprocess = subprocess.check_output(result_2.split())
-
-
+        
         with open("tmp/output_file_1") as fh1:
             for line in fh1.readlines():
-                if line.startswith("lcl"):
-                    a.append(line.split()[0])           #List a stores the first index headers (db values)
-                if line.startswith("lcl"):
-                    a1.append(line.split()[1])          #List a1 stores the second index headers (query headers)
-
+                a.append(line.split()[0])
+                a1.append(line.split()[1])
+                    
         for i,j in zip(a,a1):
             output_list_1.append(i+"\t"+j+"\n")
-
+            
         with open("tmp/output_file_2") as fh2:
             for line in fh2.readlines():
-                if line.startswith("lcl"):
-                    b.append(line.split()[0])           #List b stores the first index headers (db values)
-                if line.startswith("lcl"):
-                    b1.append(line.split()[1])          #List b1 stores the second index headers (query headers)
-
-        for i,j in zip(b,b1):
-            output_list_2.append(j+"\t"+i+"\n")         #Inversing the list in order to find common things bw output_list_1 and output_list_2
-
-        for x in output_list_1:                         #Checking common hits beween output_list_1 and output_list_2
-            if x in output_list_2:
-                output_list.append(x)                   #Contains only reciprocal blast hits
-
-    elif input_sequence_type == "p":  #For protein(p)
-
-        file_1_db = "makeblastdb -in " + str(file_one) + " -dbtype prot -out tmp/db1"   #Creating db for input1
-        file_1_db_subprocess = subprocess.check_output(file_1_db.split())
-
-        result_1 = "blastp -db tmp/db1 -query " + str(file_two) + " -max_target_seqs 1 -max_hsps 1 -outfmt 6 -out "+str(output_file) #Output for query = input2 and db = input1
-        result_1_subprocess = subprocess.check_output(result_1.split())
-
-        file_2_db = "makeblastdb -in " + str(file_two) + " -dbtype prot -out tmp/db2"   #Creating db for input2
-        file_2_db_subprocess = subprocess.check_output(file_2_db.split())
-
-        result_2 = "blastp -db tmp/db2 -query " + str(file_one) + " -max_target_seqs 1 -max_hsps 1 -outfmt 6 -out "+str(output_file) #Output for query = input1 and db = input2
-        result_2_subprocess = subprocess.check_output(result_2.split())
-
-        with open("tmp/output_file_1") as fh1:
-            for line in fh1.readlines():
-                if line.startswith("lcl"):
-                    a.append(line.split()[0])
-                if line.startswith("lcl"):
-                    a1.append(line.split()[1])
-
-        for i,j in zip(a,a1):
-            output_list_1.append(i+"\t"+j+"\n")
-
-        with open("tmp/output_file_2") as fh2:
-            for line in fh2.readlines():
-                if line.startswith("lcl"):
-                    b.append(line.split()[0])
-                if line.startswith("lcl"):
-                    b1.append(line.split()[1])
-
+                b.append(line.split()[0])
+                b1.append(line.split()[1])
+                    
         for i,j in zip(b,b1):
             output_list_2.append(j+"\t"+i+"\n")
-
+            
         for x in output_list_1:         #Checking common hits beween output_list_1 and output_list_2
             if x in output_list_2:
                 output_list.append(x)   #Contains only reciprocal blast hits
-
     return(output_list)
 
 def main():
@@ -107,26 +63,24 @@ def main():
     parser.add_argument("-i1", help = "Takes in input sequence 1.")
     parser.add_argument("-i2", help = "Takes in input sequence 2.")
     parser.add_argument("-o", help = "Your output file.")
-    parser.add_argument("-t", help = "n for nucleotide sequence, p for protein sequence.")
     args = parser.parse_args()
 
     file_one = args.i1             #Populating the variables
     file_two = args.i2
     output_file = args.o
-    input_sequence_type = args.t
 
     # Subprocess code
 
     '''
     output_list is a list of reciprocal BLAST hits. Each element is a tab
     separated pair of gene names. Eg:
-    ["lcl|AM421808.1_cds_CAM09336.1_10	lcl|AE002098.2_cds_NMB0033_33", "lcl|AM421808.1_cds_CAM09337.1_11	lcl|AE002098.2_cds_NMB0034_34", "lcl|AM421808.1_cds_CAM09338.1_12	lcl|AE002098.2_cds_NMB0035_35", "lcl|AM421808.1_cds_CAM09339.1_13	lcl|AE002098.2_cds_NMB0036_36", ...]
+    ["A0A5S6MBM4|A0A5S6MBM4_XENTR	XBXT10g000564|mRNA002866|runx1t1", "A0A6I8Q0X6|A0A6I8Q0X6_XENTR	XBXT10g012953|mRNA064117|ano10", "A0A6I8Q7J7|A0A6I8Q7J7_XENTR	XBXT10g020004|mRNA098774|LOC101733477", ...]
     '''
 
     make_dir = "mkdir tmp"
     subprocess.call(make_dir.split())
 
-    output_list = get_reciprocal_hits(file_one, file_two, input_sequence_type)
+    output_list = get_reciprocal_hits(file_one, file_two)
     with open(output_file, 'w') as output_fh:
         for ortholog_pair in output_list:
             output_fh.write(ortholog_pair)
@@ -136,3 +90,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+    
